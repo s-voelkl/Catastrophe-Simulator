f16d8e648f55a5e4fc9c5945566f2aa645a285f8
Simon Völkl <svoelkl@witron.de>
2025-06-10 18:56:47 +0200

    updated refs.bib, added euklidean distance method, gitignore fix

--- helper_functions.py
+++ helper_functions.py
@@ -6,6 +6,13 @@ def manhattan_distance(coord1: Tuple[int, int], coord2: Tuple[int, int]) -> int:
     return abs(coord1[0] - coord2[0]) + abs(coord1[1] - coord2[1])
 
 
+def euklidean_distance(coord1: Tuple[int, int], coord2: Tuple[int, int]) -> float:
+    # calculate the Euclidean distance between two coordinates
+    return round(
+        ((coord1[0] - coord2[0]) ** 2 + (coord1[1] - coord2[1]) ** 2) ** 0.5, 2
+    )
+
+
 def sort_dict_by_val_asc(frontier: Dict[Any, int]) -> List[Any]:
     # sort the frontier by f(n) ascending
     return sorted(frontier.items(), key=lambda item: item[1])
